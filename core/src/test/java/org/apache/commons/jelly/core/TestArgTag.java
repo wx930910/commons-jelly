5468a42a5286324dd159e86949a447c7ebcbda9f
Wang Xiao <wx19930910@gmail.com>
2021-02-14 17:14:06 -0500

    refactor all cases

--- core/src/test/java/org/apache/commons/jelly/core/TestArgTag.java
+++ core/src/test/java/org/apache/commons/jelly/core/TestArgTag.java
@@ -16,14 +16,13 @@
  */
 package org.apache.commons.jelly.core;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.TestSuite;
-
-import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.JellyException;
-import org.apache.commons.jelly.JellyTagException;
 import org.apache.commons.jelly.Script;
 import org.apache.commons.jelly.TagSupport;
 import org.apache.commons.jelly.XMLOutput;
@@ -31,221 +30,219 @@ import org.apache.commons.jelly.tags.core.ArgTag;
 import org.apache.commons.jelly.tags.core.ArgTagParent;
 import org.apache.commons.jelly.test.BaseJellyTest;
 
+import junit.framework.TestSuite;
+
 /**
  * @author Rodney Waldhoff
  * @version $Revision$ $Date$
  */
 public class TestArgTag extends BaseJellyTest {
 
-    public TestArgTag(String name) {
-        super(name);
-    }
+	public Script mockScript1() throws Exception {
+		Script mockInstance = mock(Script.class);
+		when(mockInstance.compile()).thenReturn(mockInstance);
+		return mockInstance;
+	}
 
-    public static TestSuite suite() throws Exception {
-        return new TestSuite(TestArgTag.class);
-    }
+	public TestArgTag(String name) {
+		super(name);
+	}
 
-    public void setUp() throws Exception {
-        super.setUp();
-        parentTag = new MockArgTagParent();
-        argTag = new ArgTag();
-        argTag.setContext(getJellyContext());
-        argTag.setParent(parentTag);
-        argTag.setBody(new MockScript());
-    }
+	public static TestSuite suite() throws Exception {
+		return new TestSuite(TestArgTag.class);
+	}
 
-    public void tearDown() throws Exception {
-        super.tearDown();
-        parentTag = null;
-        argTag = null;
-    }
+	public void setUp() throws Exception {
+		super.setUp();
+		parentTag = new MockArgTagParent();
+		argTag = new ArgTag();
+		argTag.setContext(getJellyContext());
+		argTag.setParent(parentTag);
+		argTag.setBody(mockScript1());
+	}
 
-    public void testToBooleanFromString() throws Exception {
-        argTag.setType("boolean");
-        argTag.setValue("true");
-        argTag.doTag(getXMLOutput());
-        assertEquals(Boolean.TYPE,parentTag.getType(0));
-        assertEquals(Boolean.TRUE,parentTag.getValue(0));
-    }
+	public void tearDown() throws Exception {
+		super.tearDown();
+		parentTag = null;
+		argTag = null;
+	}
 
-    public void testToCharFromString() throws Exception {
-        argTag.setType("char");
-        argTag.setValue("X");
-        argTag.doTag(getXMLOutput());
-        assertEquals(Character.TYPE,parentTag.getType(0));
-        assertEquals(new Character('X'),parentTag.getValue(0));
-    }
+	public void testToBooleanFromString() throws Exception {
+		argTag.setType("boolean");
+		argTag.setValue("true");
+		argTag.doTag(getXMLOutput());
+		assertEquals(Boolean.TYPE, parentTag.getType(0));
+		assertEquals(Boolean.TRUE, parentTag.getValue(0));
+	}
 
-    public void testToByteFromString() throws Exception {
-        argTag.setType("byte");
-        argTag.setValue("17");
-        argTag.doTag(getXMLOutput());
-        assertEquals(Byte.TYPE,parentTag.getType(0));
-        assertEquals(new Byte((byte)17),parentTag.getValue(0));
-    }
+	public void testToCharFromString() throws Exception {
+		argTag.setType("char");
+		argTag.setValue("X");
+		argTag.doTag(getXMLOutput());
+		assertEquals(Character.TYPE, parentTag.getType(0));
+		assertEquals(new Character('X'), parentTag.getValue(0));
+	}
 
-    public void testToByteFromNumber() throws Exception {
-        argTag.setType("byte");
-        argTag.setValue(new Double(17.3d));
-        argTag.doTag(getXMLOutput());
-        assertEquals(Byte.TYPE,parentTag.getType(0));
-        assertEquals(new Byte((byte)17),parentTag.getValue(0));
-    }
+	public void testToByteFromString() throws Exception {
+		argTag.setType("byte");
+		argTag.setValue("17");
+		argTag.doTag(getXMLOutput());
+		assertEquals(Byte.TYPE, parentTag.getType(0));
+		assertEquals(new Byte((byte) 17), parentTag.getValue(0));
+	}
 
-    public void testToShortFromString() throws Exception {
-        argTag.setType("short");
-        argTag.setValue("17");
-        argTag.doTag(getXMLOutput());
-        assertEquals(Short.TYPE,parentTag.getType(0));
-        assertEquals(new Short((short)17),parentTag.getValue(0));
-    }
+	public void testToByteFromNumber() throws Exception {
+		argTag.setType("byte");
+		argTag.setValue(new Double(17.3d));
+		argTag.doTag(getXMLOutput());
+		assertEquals(Byte.TYPE, parentTag.getType(0));
+		assertEquals(new Byte((byte) 17), parentTag.getValue(0));
+	}
 
-    public void testToShortFromNumber() throws Exception {
-        argTag.setType("short");
-        argTag.setValue(new Double(17.3d));
-        argTag.doTag(getXMLOutput());
-        assertEquals(Short.TYPE,parentTag.getType(0));
-        assertEquals(new Short((short)17),parentTag.getValue(0));
-    }
+	public void testToShortFromString() throws Exception {
+		argTag.setType("short");
+		argTag.setValue("17");
+		argTag.doTag(getXMLOutput());
+		assertEquals(Short.TYPE, parentTag.getType(0));
+		assertEquals(new Short((short) 17), parentTag.getValue(0));
+	}
 
-    public void testToIntFromString() throws Exception {
-        argTag.setType("int");
-        argTag.setValue("17");
-        argTag.doTag(getXMLOutput());
-        assertEquals(Integer.TYPE,parentTag.getType(0));
-        assertEquals(new Integer(17),parentTag.getValue(0));
-    }
+	public void testToShortFromNumber() throws Exception {
+		argTag.setType("short");
+		argTag.setValue(new Double(17.3d));
+		argTag.doTag(getXMLOutput());
+		assertEquals(Short.TYPE, parentTag.getType(0));
+		assertEquals(new Short((short) 17), parentTag.getValue(0));
+	}
 
-    public void testToIntFromNumber() throws Exception {
-        argTag.setType("int");
-        argTag.setValue(new Double(17.3d));
-        argTag.doTag(getXMLOutput());
-        assertEquals(Integer.TYPE,parentTag.getType(0));
-        assertEquals(new Integer(17),parentTag.getValue(0));
-    }
+	public void testToIntFromString() throws Exception {
+		argTag.setType("int");
+		argTag.setValue("17");
+		argTag.doTag(getXMLOutput());
+		assertEquals(Integer.TYPE, parentTag.getType(0));
+		assertEquals(new Integer(17), parentTag.getValue(0));
+	}
 
-    public void testToFloatFromString() throws Exception {
-        argTag.setType("float");
-        argTag.setValue("17.3");
-        argTag.doTag(getXMLOutput());
-        assertEquals(Float.TYPE,parentTag.getType(0));
-        assertEquals(new Float((float)17.3),parentTag.getValue(0));
-    }
+	public void testToIntFromNumber() throws Exception {
+		argTag.setType("int");
+		argTag.setValue(new Double(17.3d));
+		argTag.doTag(getXMLOutput());
+		assertEquals(Integer.TYPE, parentTag.getType(0));
+		assertEquals(new Integer(17), parentTag.getValue(0));
+	}
 
-    public void testToFloatFromNumber() throws Exception {
-        argTag.setType("float");
-        argTag.setValue(new Double(17.3d));
-        argTag.doTag(getXMLOutput());
-        assertEquals(Float.TYPE,parentTag.getType(0));
-        assertEquals(new Float((float)17.3),parentTag.getValue(0));
-    }
+	public void testToFloatFromString() throws Exception {
+		argTag.setType("float");
+		argTag.setValue("17.3");
+		argTag.doTag(getXMLOutput());
+		assertEquals(Float.TYPE, parentTag.getType(0));
+		assertEquals(new Float((float) 17.3), parentTag.getValue(0));
+	}
 
-    public void testToLongFromString() throws Exception {
-        argTag.setType("long");
-        argTag.setValue("17");
-        argTag.doTag(getXMLOutput());
-        assertEquals(Long.TYPE,parentTag.getType(0));
-        assertEquals(new Long(17),parentTag.getValue(0));
-    }
+	public void testToFloatFromNumber() throws Exception {
+		argTag.setType("float");
+		argTag.setValue(new Double(17.3d));
+		argTag.doTag(getXMLOutput());
+		assertEquals(Float.TYPE, parentTag.getType(0));
+		assertEquals(new Float((float) 17.3), parentTag.getValue(0));
+	}
 
-    public void testToLongFromNumber() throws Exception {
-        argTag.setType("long");
-        argTag.setValue(new Double(17.3d));
-        argTag.doTag(getXMLOutput());
-        assertEquals(Long.TYPE,parentTag.getType(0));
-        assertEquals(new Long(17),parentTag.getValue(0));
-    }
+	public void testToLongFromString() throws Exception {
+		argTag.setType("long");
+		argTag.setValue("17");
+		argTag.doTag(getXMLOutput());
+		assertEquals(Long.TYPE, parentTag.getType(0));
+		assertEquals(new Long(17), parentTag.getValue(0));
+	}
 
-    public void testToDoubleFromString() throws Exception {
-        argTag.setType("double");
-        argTag.setValue("17.3");
-        argTag.doTag(getXMLOutput());
-        assertEquals(Double.TYPE,parentTag.getType(0));
-        assertEquals(new Double(17.3),parentTag.getValue(0));
-    }
+	public void testToLongFromNumber() throws Exception {
+		argTag.setType("long");
+		argTag.setValue(new Double(17.3d));
+		argTag.doTag(getXMLOutput());
+		assertEquals(Long.TYPE, parentTag.getType(0));
+		assertEquals(new Long(17), parentTag.getValue(0));
+	}
 
-    public void testToDoubleFromNumber() throws Exception {
-        argTag.setType("double");
-        argTag.setValue(new Long(17L));
-        argTag.doTag(getXMLOutput());
-        assertEquals(Double.TYPE,parentTag.getType(0));
-        assertEquals(new Double(17),parentTag.getValue(0));
-    }
+	public void testToDoubleFromString() throws Exception {
+		argTag.setType("double");
+		argTag.setValue("17.3");
+		argTag.doTag(getXMLOutput());
+		assertEquals(Double.TYPE, parentTag.getType(0));
+		assertEquals(new Double(17.3), parentTag.getValue(0));
+	}
 
-    public void testToPrimitiveFromNull() throws Exception {
-        String[] types = { "boolean", "char", "byte", "short", "int", "float", "long", "double" };
-        for(int i=0;i<types.length;i++) {
-            argTag.setType(types[i]);
-            argTag.setValue(null);
-            try {
-                argTag.doTag(getXMLOutput());
-                fail("Expected JellyException");
-            } catch (JellyException e) {
-                // expected
-            }
-        }
-    }
+	public void testToDoubleFromNumber() throws Exception {
+		argTag.setType("double");
+		argTag.setValue(new Long(17L));
+		argTag.doTag(getXMLOutput());
+		assertEquals(Double.TYPE, parentTag.getType(0));
+		assertEquals(new Double(17), parentTag.getValue(0));
+	}
 
-    public void testFromNull() throws Exception {
-        Class[] types = { Boolean.class, Character.class, Byte.class, Short.class, Integer.class, Float.class, Long.class, Double.class, String.class, Object.class };
-        for(int i=0;i<types.length;i++) {
-            argTag.setType(types[i].getName());
-            argTag.setValue(null);
-            argTag.doTag(getXMLOutput());
-            assertEquals(types[i],parentTag.getType(i));
-            assertNull(parentTag.getValue(i));
-        }
-    }
-    
-   
-    public void testToObjectArray() throws Exception {
-        argTag.setType(Object[].class.getName());
-        argTag.setValue(new Object[] {"testObjectArray"});
-        argTag.doTag(getXMLOutput());
-        assertEquals(Object[].class,parentTag.getType(0));
-        assertEquals("testObjectArray",((Object[]) parentTag.getValue(0))[0]);
-    }
+	public void testToPrimitiveFromNull() throws Exception {
+		String[] types = { "boolean", "char", "byte", "short", "int", "float", "long", "double" };
+		for (int i = 0; i < types.length; i++) {
+			argTag.setType(types[i]);
+			argTag.setValue(null);
+			try {
+				argTag.doTag(getXMLOutput());
+				fail("Expected JellyException");
+			} catch (JellyException e) {
+				// expected
+			}
+		}
+	}
 
-    public void testToObjectArrayClass() throws Exception {
-        argTag.setType(Class.class.getName());
-        argTag.setValue(Object[].class.getName());
-        argTag.doTag(getXMLOutput());
-        assertEquals(Class.class,parentTag.getType(0));
-        assertEquals(Object[].class,parentTag.getValue(0));
-    }
+	public void testFromNull() throws Exception {
+		Class[] types = { Boolean.class, Character.class, Byte.class, Short.class, Integer.class, Float.class,
+				Long.class, Double.class, String.class, Object.class };
+		for (int i = 0; i < types.length; i++) {
+			argTag.setType(types[i].getName());
+			argTag.setValue(null);
+			argTag.doTag(getXMLOutput());
+			assertEquals(types[i], parentTag.getType(i));
+			assertNull(parentTag.getValue(i));
+		}
+	}
 
+	public void testToObjectArray() throws Exception {
+		argTag.setType(Object[].class.getName());
+		argTag.setValue(new Object[] { "testObjectArray" });
+		argTag.doTag(getXMLOutput());
+		assertEquals(Object[].class, parentTag.getType(0));
+		assertEquals("testObjectArray", ((Object[]) parentTag.getValue(0))[0]);
+	}
 
-    private MockArgTagParent parentTag = null;
-    private ArgTag argTag = null;
+	public void testToObjectArrayClass() throws Exception {
+		argTag.setType(Class.class.getName());
+		argTag.setValue(Object[].class.getName());
+		argTag.doTag(getXMLOutput());
+		assertEquals(Class.class, parentTag.getType(0));
+		assertEquals(Object[].class, parentTag.getValue(0));
+	}
 
-    class MockArgTagParent extends TagSupport implements ArgTagParent {
-        public void addArgument(Class type, Object value) {
-            typeList.add(type);
-            valueList.add(value);
-        }
+	private MockArgTagParent parentTag = null;
+	private ArgTag argTag = null;
 
-        public void doTag(XMLOutput output)  {
-        }
+	class MockArgTagParent extends TagSupport implements ArgTagParent {
+		public void addArgument(Class type, Object value) {
+			typeList.add(type);
+			valueList.add(value);
+		}
 
-        private Class getType(int i) {
-            return (Class)(typeList.get(i));
-        }
+		public void doTag(XMLOutput output) {
+		}
 
-        private Object getValue(int i) {
-            return valueList.get(i);
-        }
+		private Class getType(int i) {
+			return (Class) (typeList.get(i));
+		}
 
-        private List typeList = new ArrayList();
-        private List valueList = new ArrayList();
-    }
+		private Object getValue(int i) {
+			return valueList.get(i);
+		}
 
-    class MockScript implements Script {
-        public Script compile() throws JellyException {
-            return this;
-        }
-
-        public void run(JellyContext context, XMLOutput output) throws JellyTagException {
-        }
-    }
+		private List typeList = new ArrayList();
+		private List valueList = new ArrayList();
+	}
 
 }
--- core/src/test/java/org/apache/commons/jelly/core/TestIncludeTag.java
+++ core/src/test/java/org/apache/commons/jelly/core/TestIncludeTag.java
@@ -16,16 +16,19 @@
  */
 package org.apache.commons.jelly.core;
 
-import java.net.URL;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.spy;
 
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.net.URL;
 
 import org.apache.commons.jelly.Jelly;
 import org.apache.commons.jelly.JellyContext;
 import org.apache.commons.jelly.Script;
-import org.apache.commons.jelly.TagLibrary;
 import org.apache.commons.jelly.XMLOutput;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Makes sure that nested includes work correctly
@@ -35,116 +38,94 @@ import org.apache.commons.jelly.XMLOutput;
  */
 public class TestIncludeTag extends TestCase {
 
-    Jelly jelly = null;
-    JellyContext context = null;
-    XMLOutput xmlOutput = null;
+	public JellyContext mockJellyContext1() {
+		JellyContext mockInstance = spy(JellyContext.class);
+		doAnswer((stubInvo) -> {
+			String namespaceURI = stubInvo.getArgument(0);
+			if (namespaceURI.equals("jelly:core")) {
+				return stubInvo.callRealMethod();
+			}
+			throw new NoClassDefFoundError("Unexpected tag library uri: " + namespaceURI);
+		}).when(mockInstance).getTagLibrary(any());
+		return mockInstance;
+	}
 
-    public TestIncludeTag(String name) {
-        super(name);
-    }
+	Jelly jelly = null;
+	JellyContext context = null;
+	XMLOutput xmlOutput = null;
 
-    public static TestSuite suite() throws Exception {
-        return new TestSuite(TestIncludeTag.class);
-    }
+	public TestIncludeTag(String name) {
+		super(name);
+	}
 
-    public void setUp(String scriptName) throws Exception {
-        URL url = this.getClass().getResource(scriptName);
-        if ( url == null ) {
-            throw new Exception(
-                "Could not find Jelly script: " + scriptName
-                + " in package of class: " + this.getClass().getName()
-            );
-        }
-        setUpFromURL(url);
-    }
+	public static TestSuite suite() throws Exception {
+		return new TestSuite(TestIncludeTag.class);
+	}
 
-    public void setUpFromURL(URL url) throws Exception {
-        context = new CoreTaglibOnlyContext();
-        xmlOutput = XMLOutput.createDummyXMLOutput();
+	public void setUp(String scriptName) throws Exception {
+		URL url = this.getClass().getResource(scriptName);
+		if (url == null) {
+			throw new Exception("Could not find Jelly script: " + scriptName + " in package of class: "
+					+ this.getClass().getName());
+		}
+		setUpFromURL(url);
+	}
 
-        jelly = new Jelly();
+	public void setUpFromURL(URL url) throws Exception {
+		context = mockJellyContext1();
+		xmlOutput = XMLOutput.createDummyXMLOutput();
 
-        jelly.setUrl(url);
+		jelly = new Jelly();
 
-        String exturl = url.toExternalForm();
-        int lastSlash = exturl.lastIndexOf("/");
-        String extBase = exturl.substring(0,lastSlash+1);
-        URL baseurl = new URL(extBase);
-        context.setCurrentURL(baseurl);
-    }
+		jelly.setUrl(url);
 
-    public void testInnermost() throws Exception {
-        // performs no includes
-        setUp("c.jelly");
-        Script script = jelly.compileScript();
-        script.run(context,xmlOutput);
-        assertTrue("should have set 'c' variable to 'true'",
-                   context.getVariable("c").equals("true"));
-    }
+		String exturl = url.toExternalForm();
+		int lastSlash = exturl.lastIndexOf("/");
+		String extBase = exturl.substring(0, lastSlash + 1);
+		URL baseurl = new URL(extBase);
+		context.setCurrentURL(baseurl);
+	}
 
-    public void testMiddle() throws Exception {
-        // performs one include
-        setUp("b.jelly");
-        Script script = jelly.compileScript();
-        script.run(context,xmlOutput);
-        assertTrue("should have set 'c' variable to 'true'",
-                   context.getVariable("c").equals("true"));
-        assertTrue("should have set 'b' variable to 'true'",
-                   context.getVariable("b").equals("true"));
-    }
+	public void testInnermost() throws Exception {
+		// performs no includes
+		setUp("c.jelly");
+		Script script = jelly.compileScript();
+		script.run(context, xmlOutput);
+		assertTrue("should have set 'c' variable to 'true'", context.getVariable("c").equals("true"));
+	}
 
-    public void testOutermost() throws Exception {
-        // performs one nested include
-        setUp("a.jelly");
-        Script script = jelly.compileScript();
-        script.run(context,xmlOutput);
-        assertTrue("should have set 'c' variable to 'true'",
-                   context.getVariable("c").equals("true"));
-        assertTrue("should have set 'b' variable to 'true'",
-                   context.getVariable("b").equals("true"));
-        assertTrue("should have set 'a' variable to 'true'",
-                   context.getVariable("a").equals("true"));
-    }
+	public void testMiddle() throws Exception {
+		// performs one include
+		setUp("b.jelly");
+		Script script = jelly.compileScript();
+		script.run(context, xmlOutput);
+		assertTrue("should have set 'c' variable to 'true'", context.getVariable("c").equals("true"));
+		assertTrue("should have set 'b' variable to 'true'", context.getVariable("b").equals("true"));
+	}
 
-    /**
-     * Insure that includes happen correctly when Jelly scripts
-     * are referenced as a file (rather than as a classpath
-     * element).  Specifically checks to make sure includes succeed
-     * when the initial script is not in the user.dir directory.
-     */
-    public void testFileInclude() throws Exception {
-        // testing outermost
-        setUpFromURL(new URL("file:src/test/resources/org/apache/commons/jelly/core/a.jelly"));
-        Script script = jelly.compileScript();
-        script.run(context,xmlOutput);
-        assertTrue("should have set 'c' variable to 'true'",
-                   context.getVariable("c").equals("true"));
-        assertTrue("should have set 'b' variable to 'true'",
-                   context.getVariable("b").equals("true"));
-        assertTrue("should have set 'a' variable to 'true'",
-                   context.getVariable("a").equals("true"));
-    }
+	public void testOutermost() throws Exception {
+		// performs one nested include
+		setUp("a.jelly");
+		Script script = jelly.compileScript();
+		script.run(context, xmlOutput);
+		assertTrue("should have set 'c' variable to 'true'", context.getVariable("c").equals("true"));
+		assertTrue("should have set 'b' variable to 'true'", context.getVariable("b").equals("true"));
+		assertTrue("should have set 'a' variable to 'true'", context.getVariable("a").equals("true"));
+	}
 
-    private class CoreTaglibOnlyContext extends JellyContext {
-
-        /**
-         * This implementations makes sure that only "jelly:core"
-         * taglib is instantiated, insuring that "optional" dependencies
-         * are not inadvertantly called.  Specifically addresses a bug
-         * in older Jelly dev versions where a nested include
-         * would trigger instantiation of all tag libraries.
-         *
-         * @param namespaceURI
-         * @return
-         */
-        public TagLibrary getTagLibrary(String namespaceURI)  {
-            if (namespaceURI.equals("jelly:core")) {
-                return super.getTagLibrary(namespaceURI);
-            }
-            throw new NoClassDefFoundError("Unexpected tag library uri: " +
-                                               namespaceURI);
-        }
-
-    }
+	/**
+	 * Insure that includes happen correctly when Jelly scripts are referenced as a
+	 * file (rather than as a classpath element). Specifically checks to make sure
+	 * includes succeed when the initial script is not in the user.dir directory.
+	 */
+	public void testFileInclude() throws Exception {
+		// testing outermost
+		setUpFromURL(new URL("file:src/test/resources/org/apache/commons/jelly/core/a.jelly"));
+		Script script = jelly.compileScript();
+		script.run(context, xmlOutput);
+		assertTrue("should have set 'c' variable to 'true'", context.getVariable("c").equals("true"));
+		assertTrue("should have set 'b' variable to 'true'", context.getVariable("b").equals("true"));
+		assertTrue("should have set 'a' variable to 'true'", context.getVariable("a").equals("true"));
+	}
 
 }
